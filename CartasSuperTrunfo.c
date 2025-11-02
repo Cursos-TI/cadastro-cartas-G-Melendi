e76c34fbbeb8b86ee1d0a2059e456d9eadca990e
G-Melendi <guilherme.mdm@hotmail.com>
2025-11-02 16:03:44 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,13 +5,13 @@ int main() {
     char estado1[3], codigo1[4], nomeCidade1[50];
     int pontosTuristicos1;
     unsigned long int populacao1;
-    float area1, pib1, DensPop1, PIBPerCapita1;
+    float area1, pib1, DensPop1, PIBPerCapita1, InvDens1, SuperPoder1;
 
     //Declaração das variáveis da Carta 2
     char estado2[3], codigo2[4], nomeCidade2[50];
     int pontosTuristicos2;
     unsigned long int populacao2;
-    float area2, pib2, DensPop2, PIBPerCapita2;
+    float area2, pib2, DensPop2, PIBPerCapita2, InvDens2, SuperPoder2;
 
     //A definição das variaveis poderia ter sido feito em um bloco apenas, todos os chars em uma linha, assim como todos os floats, etc, 
     //mas para manter levemente ortganizado e para facilitar o entendimento, manteremos separados. 
@@ -42,6 +42,8 @@ int main() {
     
     DensPop1 = (float)populacao1 / area1;
     PIBPerCapita1 = pib1 * 1000000000 / (float)populacao1;
+    InvDens1 = 1 / DensPop1;
+    SuperPoder1 = (float)populacao1 + (float)area1 + (float)pib1 + (float)pontosTuristicos1 + (float)PIBPerCapita1 + (float)InvDesn1;
     
     //Leitura dos dados da Carta 2
     printf("\n Vamos cadastrar a Cidade 2?\n");
@@ -68,7 +70,8 @@ int main() {
     
     DensPop2 = (float)populacao2 / area2;
     PIBPerCapita2 = pib2 * 1000000000 / (float)populacao2;
-    
+    InvDens2 = 1 / DensPop2;
+    SuperPoder2 = (float)populacao2 + (float)area2 + (float)pib2 + (float)pontosTuristicos2 + (float)PIBPerCapita2 + (float)InvDesn2;
 
     //Exibição dos dados da Carta 1
     printf("\n\nCidade 1\n");
